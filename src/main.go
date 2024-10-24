3cc8f42c564e5d316b3f861ca1ed0382a3483eaa
Dmitry Andreev <andreev.dp@bk.ru>
2024-10-24 15:45:57 +0400

    feat: add concurrency

--- src/main.go
+++ src/main.go
@@ -3,8 +3,9 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
 	"path/filepath"
+	"sync"
+	"time"
 
 	"github.com/Helltale/vk-parser-program/config"
 	"github.com/Helltale/vk-parser-program/internal/fetcher"
@@ -13,7 +14,6 @@ import (
 )
 
 func main() {
-
 	conf, err := config.NewConfig()
 	if err != nil {
 		log.Fatalf("config error: %v\n", err)
@@ -32,17 +32,30 @@ func main() {
 	flagManager := flags.NewFlagManager()
 	flagEntry := flagManager.FlagHandler(logger)
 
-	response, err := fetcher.Init(
-		flagEntry.Flag,
-		flagEntry.Value[0], //заглушка
-		conf,
-		logger,
-	)
-	if err != nil {
-		os.Exit(1)
+	var wg sync.WaitGroup
+	sem := make(chan struct{}, conf.AppMaxGoroutine)
+
+	for _, value := range flagEntry.Value {
+		wg.Add(1)
+		sem <- struct{}{}
+
+		go func(val string) {
+			defer wg.Done()
+			defer func() { <-sem }()
+
+			time.Sleep(time.Second / time.Duration(conf.AppMaxResponceToVkToSec))
+
+			response, err := fetcher.Init(flagEntry.Flag, val, conf, logger)
+			if err != nil {
+				logger.Error("error fetching data", "flag", flagEntry.Flag, "value", val, "error", err)
+				return
+			}
+
+			if err := fetcher.SaveResponseToJSON(response, filepath.Join(conf.AppResDir, fmt.Sprintf("%s_%s_response.json", flagEntry.Flag, val))); err != nil {
+				logger.Error("error saving json", "flag", flagEntry.Flag, "value", val, "error", err)
+			}
+		}(value)
 	}
 
-	if err := fetcher.SaveResponseToJSON(response, filepath.Join(conf.AppResDir, fmt.Sprintf("%s_%s_response.json", flagEntry.Flag, flagEntry.Value))); err != nil {
-		logger.Error("error saving json", "flag", flagEntry.Flag, "value", flagEntry.Value, "error", err)
-	}
+	wg.Wait()
 }
